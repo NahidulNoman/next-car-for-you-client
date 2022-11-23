7e05a80dda31c26625d17b4b281ca2ab09ffe746
NahidulNoman <nahidulna5@gmail.com>
2022-11-24 00:49:14 +0600

    blog section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
             secondary : "#3ABFF8",
             accent : "#323F5C",
             neutral: "#B9C6D7",
+            success: "#36D399",
+            error: "#F87272",
             "base-100": "#E3EBF5",
           }
         }
