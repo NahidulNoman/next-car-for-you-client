dfffbeaed1a3b1742092af6638b067ace149a813
NahidulNoman <nahidulna5@gmail.com>
2022-11-24 01:27:44 +0600

    firebase added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../UserContext/UserContext';
 
 const SignUp = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user);
     return (
         <div>
             <h1>this is sign up.</h1>
