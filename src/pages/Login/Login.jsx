0b3d5dca074885ddb9517d891df4a76ad85d387a
NahidulNoman <nahidulna5@gmail.com>
2022-11-24 02:24:00 +0600

    login and sign up page completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import loginImg from '../../assets/login.png';
 
 const Login = () => {
+
+    const handlerLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        console.log(email,password)
+    }
+
+
     return (
-        <div>
-            <h1>this is login</h1>
+        <div >
+        <h2 className="text-4xl font-bold text-primary text-center lg:m-16">|| Log In Here ||</h2>
+        <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left lg:ml-32">
+          <img src={loginImg} className="hidden lg:block" alt="" />
         </div>
+        <div className="card w-full max-w-sm shadow-2xl bg-white">
+          <form onSubmit={handlerLogin} className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text font-semibold">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="******"
+                className="input input-bordered"
+              />
+            </div>
+
+            <input
+              className="btn btn-accent w-full"
+              value="Login"
+              type="submit"
+            />
+            <p className="text-sm text-center mt-3">
+            New This Site{" "}
+            <Link className="text-secondary" to="/signup">
+              Create an account !
+            </Link>
+          </p>
+
+            <div className="divider">OR</div>
+
+            <button className="btn btn-outline w-full">
+              CONTINUE WITH GOOGLE
+            </button>
+            
+          </form>
+
+        </div>
+      </div>
+    </div>
+    </div>
     );
 };
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,14 +1,104 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../UserContext/UserContext';
+import React, { useContext } from "react";
+import { AuthContext } from "../../UserContext/UserContext";
+import loginImg from "../../assets/login.png";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    const {user} = useContext(AuthContext);
-    console.log(user);
-    return (
-        <div>
-            <h1>this is sign up.</h1>
+  const { user } = useContext(AuthContext);
+
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const select = form.select.value;
+        const password = form.password.value;
+        
+        console.log(name,email,select,password)
+    }
+
+  return (
+    <div >
+        <h2 className="text-4xl font-bold text-primary text-center lg:m-16">|| Sign Up Here ||</h2>
+        <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left lg:ml-32">
+          <img src={loginImg} className="hidden lg:block" alt="" />
         </div>
-    );
+        <div className="card w-full max-w-sm shadow-2xl bg-white">
+          <form onSubmit={handlerSubmit} className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text font-semibold">Select</span>
+              </label>
+              <select name="select" className="select input-bordered">
+                <option>User</option>
+                <option>Seller</option>
+              </select>
+            </div>
+
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text font-semibold">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="******"
+                className="input input-bordered"
+              />
+            </div>
+
+            <input
+              className="btn btn-accent w-full"
+              value="Login"
+              type="submit"
+            />
+            <p className="text-sm text-center mt-3">
+            Already Have Account{" "}
+            <Link className="text-secondary" to="/login">
+             Please Login !
+            </Link>
+          </p>
+
+            <div className="divider">OR</div>
+
+            <button className="btn btn-outline w-full">
+              CONTINUE WITH GOOGLE
+            </button>
+            
+          </form>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default SignUp;
