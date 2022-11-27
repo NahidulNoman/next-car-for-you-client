cd4ca65b037b939ec209f6113a1a85377008e349
NahidulNoman <nahidulna5@gmail.com>
2022-11-28 00:18:13 +0600

    cart design added

--- src/pages/Home/HomeCategories/HomeCategory.jsx
+++ src/pages/Home/HomeCategories/HomeCategory.jsx
@@ -5,13 +5,25 @@ import { Link } from "react-router-dom";
 // import ferrari from "../../../assets/ferrari.jpg";
 
 const HomeCategory = ({ category }) => {
-  const { category_name } = category;
+  const { category_name, image } = category;
   return (
-    <div className="card bg-white shadow-xl">
+    // <div className="card bg-white shadow-xl">
+    //   <div className="card-body">
+    //     <h2 className="card-title">Brand : {category_name}</h2>
+        // <div className="card-actions justify-end">
+        //   <Link to={`/categories/${category_name}`}><button className="btn btn-primary">Details Category</button></Link>
+        // </div>
+    //   </div>
+    // </div>
+    <div className="card bg-base-100 shadow-xl image-full">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
       <div className="card-body">
-        <h2 className="card-title">Brand : {category_name}</h2>
-        <div className="card-actions justify-end">
-          <Link to={`/categories/${category_name}`}><button className="btn btn-primary">Details Category</button></Link>
+        <h2 className="card-title text-3xl">{category_name}</h2>
+
+        <div className="card-actions justify-end mt-32">
+          <Link to={`/categories/${category_name}`}><button className="btn btn-primary text-slate-700">Details Category</button></Link>
         </div>
       </div>
     </div>
