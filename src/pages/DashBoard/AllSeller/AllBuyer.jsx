d78ff3e2e5273c243bdde108ebafe3f9e7c788a8
NahidulNoman <nahidulna5@gmail.com>
2022-12-06 21:49:39 +0600

    allseller and allbuyer modify

--- src/pages/DashBoard/AllSeller/AllBuyer.jsx
+++ src/pages/DashBoard/AllSeller/AllBuyer.jsx
@@ -3,57 +3,64 @@ import React from "react";
 import toast from "react-hot-toast";
 
 const AllBuyer = () => {
-//   const [buyers, setBuyers] = useState([]);
-  
+  //   const [buyers, setBuyers] = useState([]);
+
   const { data: allBuyers = [], refetch } = useQuery({
     queryKey: ["allBuyers"],
     queryFn: () =>
-      fetch("https://hit-the-road-server.vercel.app/allBuyers").then((res) => res.json()),
+      fetch("https://hit-the-road-server.vercel.app/allBuyers").then((res) =>
+        res.json()
+      ),
   });
-//   console.log(allBuyers);
+  //   console.log(allBuyers);
 
   const handlerDelete = (id) => {
     fetch(`https://hit-the-road-server.vercel.app/deleteBuyer/${id}`, {
-        method : 'delete'
+      method: "delete",
     })
-    .then(res => res.json())
-    .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-        if(data.deletedCount > 0){
-            toast.success('Buyer delete successfully');
-            refetch();
+        if (data.deletedCount > 0) {
+          toast.success("Buyer delete successfully");
+          refetch();
         }
-    })
-  }
- 
+      });
+  };
+
   return (
-      <div className="overflow-x-auto">
-         <h3 className='text-3xl font-semibold mt-5 mb-5'>All Buyers</h3>
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Delete</th>
+    <div className="overflow-x-auto">
+      <h3 className="text-3xl font-semibold mt-5 mb-5">All Buyers</h3>
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allBuyers?.map((buyer, i) => (
+            <tr key={i}>
+              <th>{i + 1}</th>
+              <td>{buyer.name}</td>
+              <td>{buyer.email}</td>
+              <td>{buyer.role}</td>
+              <td>
+                <button
+                  onClick={() => handlerDelete(buyer._id)}
+                  className="btn btn-accent btn-sm"
+                >
+                  delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {
-                allBuyers?.map((buyer,i) => <tr key={i}>
-                    <th>{i+1}</th>
-                    <td>{buyer.name}</td>
-                    <td>{buyer.email}</td>
-                    <td>{buyer.role}</td>
-                    <td><button 
-                    onClick={()=> handlerDelete(buyer._id)}
-                    className="btn btn-accent btn-sm">delete</button></td>
-                  </tr>)
-            }
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/pages/DashBoard/AllSeller/AllSeller.jsx
+++ src/pages/DashBoard/AllSeller/AllSeller.jsx
@@ -3,57 +3,64 @@ import React from "react";
 import toast from "react-hot-toast";
 
 const AllSeller = () => {
-//   const [buyers, setBuyers] = useState([]);
-  
+  //   const [buyers, setBuyers] = useState([]);
+
   const { data: allSellers = [], refetch } = useQuery({
     queryKey: ["allSellers"],
     queryFn: () =>
-      fetch("https://hit-the-road-server.vercel.app/allSellers").then((res) => res.json()),
+      fetch("https://hit-the-road-server.vercel.app/allSellers").then((res) =>
+        res.json()
+      ),
   });
   console.log(allSellers);
 
   const handlerDelete = (id) => {
     fetch(`https://hit-the-road-server.vercel.app/deleteSeller/${id}`, {
-        method : 'delete'
+      method: "delete",
     })
-    .then(res => res.json())
-    .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-        if(data.deletedCount > 0){
-            toast.success('Buyer delete successfully');
-            refetch();
+        if (data.deletedCount > 0) {
+          toast.success("Buyer delete successfully");
+          refetch();
         }
-    })
-  }
- 
+      });
+  };
+
   return (
-      <div className="overflow-x-auto">
-         <h3 className='text-3xl font-semibold mt-5 mb-5'>All Sellers</h3>
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Delete</th>
+    <div className="overflow-x-auto">
+      <h3 className="text-3xl font-semibold mt-5 mb-5">All Sellers</h3>
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allSellers?.map((buyer, i) => (
+            <tr key={i}>
+              <th>{i + 1}</th>
+              <td>{buyer.name}</td>
+              <td>{buyer.email}</td>
+              <td>{buyer.role}</td>
+              <td>
+                <button
+                  onClick={() => handlerDelete(buyer._id)}
+                  className="btn btn-accent btn-sm"
+                >
+                  delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {
-                allSellers?.map((buyer,i) => <tr key={i}>
-                    <th>{i+1}</th>
-                    <td>{buyer.name}</td>
-                    <td>{buyer.email}</td>
-                    <td>{buyer.role}</td>
-                    <td><button 
-                    onClick={()=> handlerDelete(buyer._id)}
-                    className="btn btn-accent btn-sm">delete</button></td>
-                  </tr>)
-            }
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
